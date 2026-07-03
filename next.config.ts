e3d8c265b65b36575f2db637f1ef00b9e4e31869
Adarsh Chaudhary <chaudharyadarsh9919@gmail.com>
2026-07-03 09:37:59 +0530

    deprecate bodysize limit feature

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,13 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+// module.exports = {
+//   experimental: {
+//     serverActions: {
+//       bodySizeLimit: "100mb",
+//     },
+//     proxyClientMaxBodySize: "100mb",
+//   },
+// };
+
 export default nextConfig;
